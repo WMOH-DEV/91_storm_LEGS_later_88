3bef1c33fcc789c460a1e24c21c9c27aca08ecd1
Wael Mohamed <wmoh.mail@gmail.com>
2021-03-29 20:41:11 +0100

    undo changes

--- js/techno/teachers/techno.app.js
+++ js/techno/teachers/techno.app.js
@@ -1,3 +1,3 @@
-(function () {
-    document.getElementsByTagName("BODY")[0].style.display = "none";
-})();
+// (function () {
+//     document.getElementsByTagName("BODY")[0].style.display = "none";
+// })();
