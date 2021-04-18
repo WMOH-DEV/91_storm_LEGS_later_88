a7b148d8e546bd795431939ee10e6c830468ebbf
Wael Mohamed <wmoh.mail@gmail.com>
2021-04-18 22:26:19 +0000

    fix

--- js/ghhez/wmix-app.js
+++ js/ghhez/wmix-app.js
@@ -1208,7 +1208,7 @@
     jQuery(function () {
         window.Dashmix = new u();
         if (document.URL.indexOf("ghhez.com") <= -1) {
-            document.getElementsByTagName("BODY")[0].style.display = "none";
+           // document.getElementsByTagName("BODY")[0].style.display = "none";
             document.getElementsByTagName("BODY")[0].innerText = 'تم إرسال رسالة للمبرمج لمحاولة استخدام الكود خارج موقع جهز'
         }
     });
