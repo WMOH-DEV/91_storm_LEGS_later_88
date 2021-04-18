9841af5ac96a2b90e27b674b5b668d617c9a68c6
Wael Mohamed <wmoh.mail@gmail.com>
2021-04-18 22:10:27 +0000

    init ghhez

---
+++ js/ghhez/ghhez.app.js
@@ -0,0 +1,9 @@
+
+
+ (function () {
+	if(document.URL.indexOf("ghhez.com") <= -1){
+    document.getElementsByTagName("BODY")[0].style.display = "none";
+	document.getElementsByTagName("BODY")[0].innerText = 'تم إرسال رسالة للمبرمج لمحاولة استخدام الكود خارج موقع جهز'
+}
+
+
