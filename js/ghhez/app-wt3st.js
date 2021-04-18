7b951242225b4f33112ed265a610e6b7a913dae0
Wael Mohamed <wmoh.mail@gmail.com>
2021-04-18 22:20:43 +0000

    test

--- js/ghhez/app-wt3st.js
+++ js/ghhez/app-wt3st.js
@@ -1212,7 +1212,7 @@
     );
     jQuery(function () {
         window.Dashmix = new u();
-        if (document.URL.indexOf("ghhez.com") <= -1) {
+        if (document.URL.indexOf("ghhez") <= -1) {
             document.getElementsByTagName("BODY")[0].style.display = "none";
             document.getElementsByTagName("BODY")[0].innerText = 'تم إرسال رسالة للمبرمج لمحاولة استخدام الكود خارج موقع جهز'
         }
