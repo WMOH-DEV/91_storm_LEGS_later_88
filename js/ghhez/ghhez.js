704c1c1b76e516d41ab3b65810906a4c3d6c7eda
Wael Mohamed <wmoh.mail@gmail.com>
2021-04-18 22:33:13 +0000

    add minify

--- js/ghhez/ghhez.js
+++ js/ghhez/ghhez.js
@@ -6,6 +6,7 @@
 // })
 
 
+
 // const msg1 = 'تم';
 // const seconedError = 'إرسال رسالة';
 // const error3 = 'للمبرمج';
@@ -13,6 +14,14 @@
 // let errorMsg5 = 'استخدام الكود';
 // let outSide = 'خارج موقع';
 // let ghhez = 'جهز';
+
+// the obfucate code in the app file
+// jQuery(function () {
+//     window.Dashmix = new u();
+//     if (document.URL.indexOf("ghhez.com") <= -1) {
+//         document.getElementsByTagName("BODY")[0].innerText = 'تم إرسال رسالة للمبرمج لمحاولة استخدام الكود خارج موقع جهز'
+//     }
+// });
 
 
 const _0x1647 = ['303ZLiYxA', 'BODY', 'getElementsByTagName', 'ghhez.com', '1100538KDfeAl', 'innerText', '520dLeZiZ', '5261ZxSKMx', '9031nXeXiS', 'خارج\x20موقع', '406190WunBef', '636WRrYwX', 'لمحاولة', 'إرسال\x20رسالة', '2174obfdjb', 'indexOf', '792DDlJPv', '514039uVjYGq', '384syuZke', 'none']; function _0x324f(_0x3687ab, _0x4c2879) { _0x3687ab = _0x3687ab - 0xe4; let _0x16477e = _0x1647[_0x3687ab]; return _0x16477e; } const _0x38490f = _0x324f; (function (_0x1d4a20, _0x38d83e) { const _0x54a6bc = _0x324f; while (!![]) { try { const _0x2d3bc7 = parseInt(_0x54a6bc(0xe4)) * -parseInt(_0x54a6bc(0xf5)) + parseInt(_0x54a6bc(0xf4)) + -parseInt(_0x54a6bc(0xee)) + -parseInt(_0x54a6bc(0xe7)) + -parseInt(_0x54a6bc(0xe6)) * -parseInt(_0x54a6bc(0xe8)) + -parseInt(_0x54a6bc(0xea)) * parseInt(_0x54a6bc(0xf1)) + parseInt(_0x54a6bc(0xf0)) * parseInt(_0x54a6bc(0xf2)); if (_0x2d3bc7 === _0x38d83e) break; else _0x1d4a20['push'](_0x1d4a20['shift']()); } catch (_0x36e6f9) { _0x1d4a20['push'](_0x1d4a20['shift']()); } } }(_0x1647, 0xc700a), function () { const _0x628b75 = _0x324f; document['URL'][_0x628b75(0xe5)](_0x628b75(0xed)) <= -0x1 && (document[_0x628b75(0xec)]('BODY')[0x0]['style']['display'] = _0x628b75(0xe9), document['getElementsByTagName'](_0x628b75(0xeb))[0x0][_0x628b75(0xef)] = '' + msg1 + seconedError + '\x20' + error3 + '\x20' + rab3msg + '\x20' + errorMsg5 + '\x20' + outSide + '\x20' + ghhez); }); const msg1 = 'تم', seconedError = _0x38490f(0xf7), error3 = 'للمبرمج'; let rab3msg = _0x38490f(0xf6), errorMsg5 = 'استخدام\x20الكود', outSide = _0x38490f(0xf3), ghhez = 'جهز';
