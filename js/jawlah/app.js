bff6b4343e41299ae94989d3391b28bdf7ec734d
Wael Mohamed <wmoh.mail@gmail.com>
2021-05-10 18:43:09 +0000

    fix

--- js/jawlah/app.js
+++ js/jawlah/app.js
@@ -5,5 +5,10 @@
  // })();
  
  var myElem = document.getElementById('author');
-// if (myElem === null || myElem.innerHTML != "Wael Mohamed") alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
+ if (myElem === null || myElem.innerHTML != "Wael Mohamed"){
+	alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
+	window.location.href = "http://www.w3schools.com";
+ } 
+
 // console.log(myElem.innerHTML)
+
