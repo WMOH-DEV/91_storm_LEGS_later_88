831262c18692dffa67a4c036257fb4ff6ca7bac2
Wael Mohamed <wmoh.mail@gmail.com>
2021-05-10 18:41:19 +0000

    fix

--- js/jawlah/app.js
+++ js/jawlah/app.js
@@ -5,5 +5,5 @@
  // })();
  
  var myElem = document.getElementById('author');
-// if (myElem === null || myElem.innerHTML !== "Wael Mohamed") alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
-console.log(myElem.innerHTML)
+// if (myElem === null || myElem.innerHTML != "Wael Mohamed") alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
+// console.log(myElem.innerHTML)
