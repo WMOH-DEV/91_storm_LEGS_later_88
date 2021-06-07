b59a1df50cb182ae33851cd8fa6d31bba62fbf0f
Wael Mohamed <wmoh.mail@gmail.com>
2021-06-07 20:27:58 +0100

    upload shms

---
+++ js/shms/app.js
@@ -0,0 +1,14 @@
+// ضع هنا أي كود تريده في الموقع
+
+ // (function () {
+     // document.getElementsByTagName("BODY")[0].style.display = "none";
+ // })();
+ 
+ var myElem = document.getElementById('author');
+ if (myElem === null || myElem.innerHTML != "Wael Mohamed"){
+	alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
+	window.location.href = "https://www.fb.com/WaelMohElSaid";
+ } 
+
+// console.log(myElem.innerHTML)
+
