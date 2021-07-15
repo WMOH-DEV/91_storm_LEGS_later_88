6bc191aa46a7707ce9c3104f351a0f41b32ce11e
WaeL M.El-Saied <58665715+WMOH-DEV@users.noreply.github.com>
2021-07-15 14:47:32 +0100

    Update app copy.js

--- js/jawlah/app copy.js
+++ js/jawlah/app copy.js
@@ -1,16 +1,4 @@
 
-(function () {
-	document.getElementsByTagName("BODY")[0].style.display = "none";
-})();
-
-(function () {
-	document.getElementsByTagName("BODY")[0].innerHTML = `<div style="text-align:center;height:100vh;    display: flex;
-	font-size:25px;
-	font-weight:bold;
-    justify-content: center;
-    align-items: center;"> Please contact The Coder -> <a href='https://www.fb.com/WaelMohElSaid'> FaceBook@WaelMohElSaid</a></div>`;
-})();
-
 
 var myElem = document.getElementById('author');
 if (myElem === null || myElem.innerHTML != "Wael Mohamed") {
@@ -20,100 +8,8 @@ if (myElem === null || myElem.innerHTML != "Wael Mohamed") {
 
 
 // This is url copyright before decode
-
-const d = "j";
-const o = "aw";
-
-var myElem = document.getElementById('author');
-
-const m = "l";
-const ai = "a";
-let http, socialMedia, extC, myName, restOfName;
-
-const n = "h";
-const dot = ".";
-const ext = "net";
-http = "http://";
-socialMedia = "fb";
-extC = "com";
-
-myName = "/wael";
-
-restOfName = 'MohElSaid'
-
-let siteUrl = d + o + m + ai + n + dot + ext;
-
-if (window.location.hostname != siteUrl) {
-    alert('domain forbidden');
-    window.location.href = http + socialMedia + dot + extC + myName + restOfName;
-}
-
-// End of if
-
-
-// decoded 
-
-const _0x1a37 = ['1kfINow', '1HqhQkV', '126806gjhnzO', 'href', '40cmZOGx', '/wael', '1244318lkJkgr', 'MohElSaid', 'com', 'net', '7019assJJL', '1bqgwjP', 'getElementById', '1cRxRVN', 'domain\x20forbidden', '180695vvxSKP', '99032uYPSoX', 'location', 'hostname', '1419xTuvUP', 'author', '371479uWazTV', '2CjUNzw']; const _0x289aec = _0x41cb; function _0x41cb(_0x665e99, _0x39054c) { return _0x41cb = function (_0x1a3787, _0x41cb28) { _0x1a3787 = _0x1a3787 - 0x1a7; let _0x1e0426 = _0x1a37[_0x1a3787]; return _0x1e0426; }, _0x41cb(_0x665e99, _0x39054c); } (function (_0x4bcdd3, _0x56e838) { const _0x194e71 = _0x41cb; while (!![]) { try { const _0x33b862 = parseInt(_0x194e71(0x1b6)) + parseInt(_0x194e71(0x1bb)) * -parseInt(_0x194e71(0x1b3)) + -parseInt(_0x194e71(0x1b5)) * parseInt(_0x194e71(0x1bc)) + -parseInt(_0x194e71(0x1bd)) * parseInt(_0x194e71(0x1a8)) + parseInt(_0x194e71(0x1aa)) * -parseInt(_0x194e71(0x1b0)) + -parseInt(_0x194e71(0x1b9)) * -parseInt(_0x194e71(0x1a7)) + parseInt(_0x194e71(0x1b1)) * parseInt(_0x194e71(0x1ac)); if (_0x33b862 === _0x56e838) break; else _0x4bcdd3['push'](_0x4bcdd3['shift']()); } catch (_0x33ba9f) { _0x4bcdd3['push'](_0x4bcdd3['shift']()); } } }(_0x1a37, 0x31e2e)); const d = 'j', o = 'aw'; var myElem = document[_0x289aec(0x1b2)](_0x289aec(0x1ba)); const m = 'l', ai = 'a'; let http, socialMedia, extC, myName, restOfName; const n = 'h', dot = '.', ext = _0x289aec(0x1af); http = 'http://', socialMedia = 'fb', extC = _0x289aec(0x1ae), myName = _0x289aec(0x1ab), restOfName = _0x289aec(0x1ad); let siteUrl = d + o + m + ai + n + dot + ext; window['location'][_0x289aec(0x1b8)] != siteUrl && (alert(_0x289aec(0x1b4)), window[_0x289aec(0x1b7)][_0x289aec(0x1a9)] = http + socialMedia + dot + extC + myName + restOfName);
-
-// End of it
-
-
-var _0x1008 = ['1232046CGwfib', 'BODY', '2713721cbxrbF', '<div\x20style=\x22text-align:center;height:100vh;\x20\x20\x20\x20display:\x20flex;\x0a\x09font-size:25px;\x0a\x09font-weight:bold;\x0a\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20align-items:\x20center;\x22>\x20Please\x20contact\x20The\x20Coder\x20->\x20<a\x20href=\x27https://www.fb.com/WaelMohElSaid\x27>\x20FaceBook@WaelMohElSaid</a></div>', '14321thbgNS', '43itnyyD', '118LGhCab', '1469024vOuVDX', '1mqBEvY', '84235Wzfksk', '36187PKmgtn', '1120061CFLQjN']; function _0x543b(_0x5deb87, _0xcc3cb8) { return _0x543b = function (_0x1008f5, _0x543bf5) { _0x1008f5 = _0x1008f5 - 0x1ca; var _0x20b453 = _0x1008[_0x1008f5]; return _0x20b453; }, _0x543b(_0x5deb87, _0xcc3cb8); } (function (_0x459d57, _0x5e359d) { var _0x5ab532 = _0x543b; while (!![]) { try { var _0x87cf44 = -parseInt(_0x5ab532(0x1d3)) * -parseInt(_0x5ab532(0x1ca)) + -parseInt(_0x5ab532(0x1cf)) * parseInt(_0x5ab532(0x1d1)) + parseInt(_0x5ab532(0x1d2)) + -parseInt(_0x5ab532(0x1d0)) * -parseInt(_0x5ab532(0x1d5)) + parseInt(_0x5ab532(0x1cb)) + -parseInt(_0x5ab532(0x1d4)) + -parseInt(_0x5ab532(0x1cd)); if (_0x87cf44 === _0x5e359d) break; else _0x459d57['push'](_0x459d57['shift']()); } catch (_0x1502b4) { _0x459d57['push'](_0x459d57['shift']()); } } }(_0x1008, 0xd91fa), function () { var _0x117f71 = _0x543b; document['getElementsByTagName'](_0x117f71(0x1cc))[0x0]['innerHTML'] = _0x117f71(0x1ce); }());
-
-// console.log(myElem.innerHTML)
-
-
-// Full code Url and redirect 
-
-const d = "j";
-const o = "aw";
-
-var myElem = document.getElementById('author');
-
-const m = "l";
-const ai = "a";
-let http, socialMedia, extC, myName, restOfName;
-
-const n = "h";
-const dot = ".";
-const ext = "net";
-http = "http://";
-socialMedia = "fb";
-extC = "com";
-
-myName = "/wael";
-
-restOfName = 'MohElSaid'
-
-let siteUrl = d + o + m + ai + n + dot + ext;
-
-
-
-(function () {
-    if (window.location.hostname != siteUrl) {
-        alert('domain forbidden');
-        window.location.href = http + socialMedia + dot + extC + myName + restOfName;
-    }
-
-    document.getElementsByTagName("BODY")[0].innerHTML = `<div style="text-align:center;height:100vh;    display: flex;
-	font-size:25px;
-	font-weight:bold;
-    justify-content: center;
-    align-items: center;"> Please contact The Coder -> <a href=${http + socialMedia + dot + extC + myName + restOfName}> FaceBook@WaelMohElSaid</a></div>`;
-
-})();
-
-
-// End of it 
-
-// after decode 
-
-const _0x48ec=['domain\x20forbidden','7EqjdhO','/wael','getElementById','100004TfIijG','35zLtUxF','>\x20FaceBook@WaelMohElSaid</a></div>','272055rwCNmY','innerHTML','1nvlwlW','author','com','33605AfDhxu','9241stsIJt','1155261FbQDaa','getElementsByTagName','http://','<div\x20style=\x22text-align:center;height:100vh;\x20\x20\x20\x20display:\x20flex;\x0a\x09font-size:25px;\x0a\x09font-weight:bold;\x0a\x20\x20\x20\x20justify-content:\x20center;\x0a\x20\x20\x20\x20align-items:\x20center;\x22>\x20Please\x20contact\x20The\x20Coder\x20->\x20<a\x20href=','278476dtnSBN','7USDPNY','MohElSaid','187282gKjFBy','location'];const _0x553ba6=_0x1594;(function(_0x33fa22,_0x5696e0){const _0x1ee074=_0x1594;while(!![]){try{const _0x3554fd=parseInt(_0x1ee074(0x19e))*-parseInt(_0x1ee074(0x192))+-parseInt(_0x1ee074(0x1a2))*-parseInt(_0x1ee074(0x193))+-parseInt(_0x1ee074(0x19b))+parseInt(_0x1ee074(0x1a1))*-parseInt(_0x1ee074(0x199))+parseInt(_0x1ee074(0x198))+-parseInt(_0x1ee074(0x18d))+-parseInt(_0x1ee074(0x18f))*-parseInt(_0x1ee074(0x194));if(_0x3554fd===_0x5696e0)break;else _0x33fa22['push'](_0x33fa22['shift']());}catch(_0x5d2746){_0x33fa22['push'](_0x33fa22['shift']());}}}(_0x48ec,0x5884c));const d='j',o='aw';function _0x1594(_0x1ce23b,_0x556a8d){return _0x1594=function(_0x48ec0b,_0x1594c0){_0x48ec0b=_0x48ec0b-0x18c;let _0x4e18e8=_0x48ec[_0x48ec0b];return _0x4e18e8;},_0x1594(_0x1ce23b,_0x556a8d);}var myElem=document[_0x553ba6(0x1a0)](_0x553ba6(0x190));const m='l',ai='a';let http,socialMedia,extC,myName,restOfName;const n='h',dot='.',ext='net';http=_0x553ba6(0x196),socialMedia='fb',extC=_0x553ba6(0x191),myName=_0x553ba6(0x19f),restOfName=_0x553ba6(0x19a);let siteUrl=d+o+m+ai+n+dot+ext;(function(){const _0x301bc9=_0x553ba6;window[_0x301bc9(0x19c)]['hostname']!=siteUrl&&(alert(_0x301bc9(0x19d)),window['location']['href']=http+socialMedia+dot+extC+myName+restOfName),document[_0x301bc9(0x195)]('BODY')[0x0][_0x301bc9(0x18e)]=_0x301bc9(0x197)+(http+socialMedia+dot+extC+myName+restOfName)+_0x301bc9(0x18c);}());
-
-// Last update with send the url email
-
-/* SmtpJS.com - v3.0.0 */
 var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
 
 const d = "j";
 const o = "aw";
@@ -132,29 +28,12 @@ myName = "/wael";
 restOfName = 'MohElSaid'
 
 let siteUrl = d + o + m + ai + n + dot + ext;
+
 let currentDomain = window.location.hostname;
 
+sendEmail();
+
 if (currentDomain != siteUrl) {
-    Email.send({
-        Host: "smtp.mailtrap.io",
-        Username: "37e7b846513b81",
-        Password: "59c873bd3f5833",
-        To: 'recipient@example.com',
-        From: "sender@example.com",
-        Subject: "site is running",
-        Body: `<html>
-        <h2>Jawlah is running</h2>
-        site name : jawlah
-        <br>
-        site Url : ${siteUrl}
-        <br>
-        Current Domain : ${currentDomain}
-        <br>
-        Time : ${new Date().toLocaleString()}
-        </html>`
-    }).then(
-        message => alert(message)
-    );
     alert('domain forbidden');
     window.location.href = http + socialMedia + dot + extC + myName + restOfName;
 }
@@ -166,6 +45,23 @@ document.getElementsByTagName("BODY")[0].innerHTML = `<div style="text-align:cen
     justify-content: center;
     align-items: center;"> Please contact The Coder -> <a href=${http + socialMedia + dot + extC + myName + restOfName}> FaceBook@WaelMohElSaid</a></div>`;
 
-    
 
+
+function sendEmail() {
+    Email.send({
+        SecureToken: "92dfb467-3295-48b5-8ce7-b4129c9f23f5",
+        To: 'w.m4ilb0x@gmail.com',
+        From: "shms@shms.app",
+        Subject: `${d + o + m + ai + n} is running`,
+        Body: `<html>
+        <h2>${siteUrl} is running</h2>
+        <br>
+        site Url : ${siteUrl}
+        <br>
+        Current Domain : ${currentDomain}
+        <br>
+        Time : ${new Date().toLocaleString()}
+        </html>`
+    });
+};
 
