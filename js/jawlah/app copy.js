a22914bc37807e5ebdd0a2b04e6a9233fbbe9857
WaeL M.El-Saied <wmoh.mail@gmail.com>
2021-10-03 03:20:31 +0100

    update

--- js/jawlah/app copy.js
+++ js/jawlah/app copy.js
@@ -1,15 +1,49 @@
-
-
-var myElem = document.getElementById('author');
+var myElem = document.getElementById("author");
 if (myElem === null || myElem.innerHTML != "Wael Mohamed") {
-	alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
-	window.location.href = "https://www.fb.com/WaelMohElSaid";
+  alert("يرجى الإلتزام بحقوق المبرمج - وائل محمد");
+  window.location.href = "https://www.fb.com/WaelMohElSaid";
 }
 
-
 // This is url copyright before decode
-var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
-
+var Email = {
+  send: function (a) {
+    return new Promise(function (n, e) {
+      (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = "Send");
+      var t = JSON.stringify(a);
+      Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+        n(e);
+      });
+    });
+  },
+  ajaxPost: function (e, n, t) {
+    var a = Email.createCORSRequest("POST", e);
+    a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
+      (a.onload = function () {
+        var e = a.responseText;
+        null != t && t(e);
+      }),
+      a.send(n);
+  },
+  ajax: function (e, n) {
+    var t = Email.createCORSRequest("GET", e);
+    (t.onload = function () {
+      var e = t.responseText;
+      null != n && n(e);
+    }),
+      t.send();
+  },
+  createCORSRequest: function (e, n) {
+    var t = new XMLHttpRequest();
+    return (
+      "withCredentials" in t
+        ? t.open(e, n, !0)
+        : "undefined" != typeof XDomainRequest
+        ? (t = new XDomainRequest()).open(e, n)
+        : (t = null),
+      t
+    );
+  },
+};
 
 const d = "j";
 const o = "aw";
@@ -25,7 +59,7 @@ extC = "com";
 
 myName = "/wael";
 
-restOfName = 'MohElSaid'
+restOfName = "MohElSaid";
 
 let siteUrl = d + o + m + ai + n + dot + ext;
 
@@ -34,26 +68,27 @@ let currentDomain = window.location.hostname;
 sendEmail();
 
 if (currentDomain != siteUrl) {
-    alert('domain forbidden');
-    window.location.href = http + socialMedia + dot + extC + myName + restOfName;
+  alert("domain forbidden");
+  window.location.href = http + socialMedia + dot + extC + myName + restOfName;
 }
 
-
-document.getElementsByTagName("BODY")[0].innerHTML = `<div style="text-align:center;height:100vh;    display: flex;
+document.getElementsByTagName(
+  "BODY"
+)[0].innerHTML = `<div style="text-align:center;height:100vh;    display: flex;
 	font-size:25px;
 	font-weight:bold;
     justify-content: center;
-    align-items: center;"> Please contact The Coder -> <a href=${http + socialMedia + dot + extC + myName + restOfName}> FaceBook@WaelMohElSaid</a></div>`;
-
-
+    align-items: center;"> Please contact The Coder -> <a href=${
+      http + socialMedia + dot + extC + myName + restOfName
+    }> FaceBook@WaelMohElSaid</a></div>`;
 
 function sendEmail() {
-    Email.send({
-        SecureToken: "92dfb467-3295-48b5-8ce7-b4129c9f23f5",
-        To: 'w.m4ilb0x@gmail.com',
-        From: "shms@shms.app",
-        Subject: `${d + o + m + ai + n} is running`,
-        Body: `<html>
+  Email.send({
+    SecureToken: "92dfb467-3295-48b5-8ce7-b4129c9f23f5",
+    To: "w.m4ilb0x@gmail.com",
+    From: "shms@shms.app",
+    Subject: `${d + o + m + ai + n} is running`,
+    Body: `<html>
         <h2>${siteUrl} is running</h2>
         <br>
         site Url : ${siteUrl}
@@ -61,7 +96,6 @@ function sendEmail() {
         Current Domain : ${currentDomain}
         <br>
         Time : ${new Date().toLocaleString()}
-        </html>`
-    });
-};
-
+        </html>`,
+  });
+}
--- js/shms/app copy.js
+++ js/shms/app copy.js
@@ -1,13 +1,48 @@
-var myElem = document.getElementById('author');
+var myElem = document.getElementById("author");
 if (myElem === null || myElem.innerHTML != "Wael Mohamed") {
-	alert('يرجى الإلتزام بحقوق المبرمج - وائل محمد');
-	window.location.href = "https://www.fb.com/WaelMohElSaid";
+  alert("يرجى الإلتزام بحقوق المبرمج - وائل محمد");
+  window.location.href = "https://www.fb.com/WaelMohElSaid";
 }
 
-// full with email send
-
-var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
-
+var Email = {
+  send: function (a) {
+    return new Promise(function (n, e) {
+      (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = "Send");
+      var t = JSON.stringify(a);
+      Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+        n(e);
+      });
+    });
+  },
+  ajaxPost: function (e, n, t) {
+    var a = Email.createCORSRequest("POST", e);
+    a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
+      (a.onload = function () {
+        var e = a.responseText;
+        null != t && t(e);
+      }),
+      a.send(n);
+  },
+  ajax: function (e, n) {
+    var t = Email.createCORSRequest("GET", e);
+    (t.onload = function () {
+      var e = t.responseText;
+      null != n && n(e);
+    }),
+      t.send();
+  },
+  createCORSRequest: function (e, n) {
+    var t = new XMLHttpRequest();
+    return (
+      "withCredentials" in t
+        ? t.open(e, n, !0)
+        : "undefined" != typeof XDomainRequest
+        ? (t = new XDomainRequest()).open(e, n)
+        : (t = null),
+      t
+    );
+  },
+};
 
 const d = "s";
 const o = "h";
@@ -22,7 +57,7 @@ extC = "com";
 
 myName = "/wael";
 
-restOfName = 'MohElSaid'
+restOfName = "MohElSaid";
 
 let siteUrl = d + o + m + ain + dot + ext;
 
@@ -31,25 +66,27 @@ let currentDomain = window.location.hostname;
 sendEmail();
 
 if (currentDomain != siteUrl) {
-    alert('domain forbidden');
-    window.location.href = http + socialMedia + dot + extC + myName + restOfName;
+  alert("domain forbidden");
+  window.location.href = http + socialMedia + dot + extC + myName + restOfName;
 }
 
-
-document.getElementsByTagName("BODY")[0].innerHTML = `<div style="text-align:center;height:100vh;    display: flex;
+document.getElementsByTagName(
+  "BODY"
+)[0].innerHTML = `<div style="text-align:center;height:100vh;    display: flex;
 	font-size:25px;
 	font-weight:bold;
     justify-content: center;
-    align-items: center;"> Please contact The Coder -> <a href=${http + socialMedia + dot + extC + myName + restOfName}> FaceBook@WaelMohElSaid</a></div>`;
-
+    align-items: center;"> Please contact The Coder -> <a href=${
+      http + socialMedia + dot + extC + myName + restOfName
+    }> FaceBook@WaelMohElSaid</a></div>`;
 
 function sendEmail() {
-    Email.send({
-        SecureToken: "92dfb467-3295-48b5-8ce7-b4129c9f23f5",
-        To: 'w.m4ilb0x@gmail.com',
-        From: "shms@shms.app",
-        Subject: "Site is running",
-        Body: `<html>
+  Email.send({
+    SecureToken: "92dfb467-3295-48b5-8ce7-b4129c9f23f5",
+    To: "w.m4ilb0x@gmail.com",
+    From: "shms@shms.app",
+    Subject: "Site is running",
+    Body: `<html>
         <h2>${siteUrl} is running</h2>
         <br>
         site Url : ${siteUrl}
@@ -57,6 +94,6 @@ function sendEmail() {
         Current Domain : ${currentDomain}
         <br>
         Time : ${new Date().toLocaleString()}
-        </html>`
-    });
-};
+        </html>`,
+  });
+}
