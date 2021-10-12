ec2d1619bec66004590ee8d5b03750dda674592a
WaeL M.El-Saied <wmoh.mail@gmail.com>
2021-10-12 19:44:57 +0100

    update

--- js/fastgo/app copy.js
+++ js/fastgo/app copy.js
@@ -39,7 +39,7 @@ var Email = {
 };
 
 var myElem = document.getElementById("author");
-if (myElem === null || myElem.innerHTML != "Wael Mohamed") {
+if (myElem === null || !myElem.textContent.includes('Wael Mohamed')) {
   alert("يرجى الإلتزام بحقوق المبرمج - وائل محمد");
   window.location.href = "https://www.fb.com/WaelMohElSaid";
 }
